483b93f31a824431b4cf43c5fd33303212fa79fd
Harsh Suvarna <harsh.suvarna@codeb.online>
2023-04-23 11:00:40 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+
+function App() {
+  return (
+    <div className="App">
+      <p className="font-bold text-amber-900">This is my Resume App</p>
+      <Test />
+    </div>
+  );
+}
+
+export default App;
